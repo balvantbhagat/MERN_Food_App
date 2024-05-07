34462c2b47e82bb7ff3c51169f7823b12719e742
BALVANT <bhagatbalvant@gmail.com>
2024-05-07 18:49:34 +0530

    frontend update

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Oreder your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor unde
+          debitis maiores ea laboriosam laudantium nostrum tenetur obcaecati
+          repellendus impedit?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
